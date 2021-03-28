08174f772849857e55a1b359e8aff14ca78830d8
Raycho Mukelov <raicho.mukelov@gmail.com>
2021-03-28 21:13:45 +0300

    Tweaked the run_image.* scripts to check for a container running the same image and remove it before running it again

--- run_image.cmd
+++ run_image.cmd
@@ -1,1 +1,5 @@
+docker ps -aq --filter name=mixcloud_fake_plays > res_img_id
+set /p Res_Img_Id=<res_img_id
+del res_img_id
+docker rm -f %Res_Img_Id%
 docker run --name mixcloud_fake_plays -d mixcloud_fake_plays
--- run_image.sh
+++ run_image.sh
@@ -1,1 +1,2 @@
+docker rm -f $(docker ps -aq --filter name=mixcloud_fake_plays)
 docker run --name mixcloud_fake_plays -d mixcloud_fake_plays
